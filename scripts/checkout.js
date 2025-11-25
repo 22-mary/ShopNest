57ddd7b19c3b3b20915db52a15c0c8d75b39fc7d
MaryGichuki <marygichuki10@gmail.com>
2025-11-25 05:32:08 +0300

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,60 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { loadProducts } from "../data/products.js";
 
 import { renderPaymentSumary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import '../data/cart-class.js'
 import '../data/car.js';
-import '../data/backend-practice.js'
-renderOrderSummary();
-renderPaymentSumary();
-renderCheckoutHeader();
+//import '../data/backend-practice.js'
+import { loadCart } from "../data/cart.js";
+Promise.all([
+  new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve('value1');
+  });
+  }),
+  new Promise((resolve)=>{
+      loadCart(()=>{
+        resolve();
+      });
+    })
+]).then(()=>{
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSumary();
+})
+
+/*new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve('value1');});
+  }).then((value)=>{
+    console.log(value);
+    return new Promise((resolve)=>{
+      loadCart(()=>{
+        resolve();
+      })
+    })
+  }).then(()=>{
+    renderOrderSummary;
+    renderPaymentSumary;
+    renderCheckoutHeader;
+  })
+
+
+
+new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve();
+  });
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSumary();
+    renderCheckoutHeader();
+    
+  });
+/*loadProducts(()=>{
+  renderOrderSummary();
+  renderPaymentSumary();
+  renderCheckoutHeader();
+
+})*/
--- test.jasmine/checkout/orderSummaryTest.js
+++ test.jasmine/checkout/orderSummaryTest.js
@@ -1,12 +1,18 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import {  loadFromStorage, cart } from "../../data/cart.js";
 import { renderPaymentSumary } from "../../scripts/checkout/paymentSummary.js";
+import { loadProducts } from "../../data/products.js";
 describe('test suite: render OrderSummary',()=>{
   const productId1= "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productId2= "15b6fc6f-327a-4ec4-896f-486349e85a3d";
   const deliveryOptionId1='1';
   const deliveryOptionId2='2';
   const deliveryOptionId3='3';
+  beforeAll((done)=>{
+    loadProducts(()=>{
+      done();
+    });
+  })
   afterEach(()=>{
     document.querySelector('.js-test-container').innerHTML='' ;
   })
