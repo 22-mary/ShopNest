627f95ac703f09ac10d6ba232789c7be4b7e65b3
MaryGichuki <marygichuki10@gmail.com>
2026-03-16 12:34:18 +0300

    header quantity in a function for it to be easier to render it

--- scripts/checkout/checkoutHeaderView.js
+++ scripts/checkout/checkoutHeaderView.js
@@ -1,5 +1,5 @@
 export function renderCheckoutHeaderHTML(itemQuantity) {
   return `
-    Checkout (<a class="return-to-home-link" href="amazon.html">${itemQuantity} item${itemQuantity !== 1 ? 's' : ''}</a>)
+    Checkout (<a class="return-to-home-link" href="amazon.html">${itemQuantity} item${itemQuantity > 1 ? 's' : ''}</a>)
   `;
 }
